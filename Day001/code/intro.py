096fb7085c81730be67a64209618663af84628f1
Ved Prakash Meena <sociallyved21@gmail.com>
2026-09-23 18:55:33 +0530

    day 1: intro script

---
+++ Day001/code/intro.py
@@ -0,0 +1,9 @@
+print("Hello, World")
+name = "Ved"
+age = 22
+hobby = "Badminton"
+
+print(type(age))
+print(type(name))
+print(type(hobby))
+print(f"My name is {name} and I am {age} years old and my hobby is playing {hobby}.")
